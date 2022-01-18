c8c985899ad7f85b89b0a00b9250707efe87305a
Christian Wolf Alves Hess <christianhess94@gmail.com>
2022-01-18 17:20:18 -0300

    initial structure & files

---
+++ src/interfaces/rest.interfaces.ts
@@ -0,0 +1,17 @@
+export interface Housing {
+  id: string;
+  address: {
+    formattedAddress: string;
+    geolocation: {
+      lat: number;
+      lng: number;
+    };
+  };
+  images: [string];
+  price: number;
+  bathrooms: number;
+  bedrooms: number;
+  parkingSpaces: number;
+  usableArea: number;
+  publish: boolean;
+}
